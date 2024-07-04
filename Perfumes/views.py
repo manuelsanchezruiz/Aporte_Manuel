0c33d8401772d6b559e7a53fcfff2bda95d3c01d
manuelsanchezruiz <msanchezr@fafi.utb.ec>
2024-07-04 10:50:32 -0500

    Actualiza configuración

--- Perfumes/views.py
+++ Perfumes/views.py
@@ -54,7 +54,7 @@ def Secion(request):
     return render(request, 'perfumes/Secion.html', {'form': form})
 
 @login_required
-def principal(request):
+def Principal(request):
     productos = Producto.objects.all()
     return render(request, 'perfumes/Principal.html', {'productos': productos})
 
